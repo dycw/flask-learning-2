05c46dba9d8e7b1b56020fd4df824ea26f5c70ad
Derek Wan <d.wan@icloud.com>
2020-11-22 00:10:13 +0800

    Add flasky/ in gitignore

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
+from typing import Any
+from typing import Tuple
+
 from flask import Flask
 from flask import render_template
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route("/")
@@ -14,3 +20,13 @@ def index() -> str:
 @app.route("/user/<name>")
 def user(name: str) -> str:
     return render_template("user.html", name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e: Any) -> Tuple[str, int]:  # noqa: U100
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e: Any) -> Tuple[str, int]:  # noqa: U100
+    return render_template("500.html"), 500
