c5304a72d9745102164297b237d2b6292fc66b24
Derek Wan <d.wan@icloud.com>
2020-11-21 23:38:02 +0800

    Add some templates

--- hello.py
+++ hello.py
@@ -1,18 +1,16 @@
 from __future__ import annotations
 
 from flask import Flask
-from flask import request
-
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index() -> str:
-    user_agent = request.headers.get("User-Agent")
-    return f"<p>Your browser is {user_agent}</p>"
+    return render_template("index.html")
 
 
 @app.route("/user/<name>")
 def user(name: str) -> str:
-    return f"<h1>Hello, {name}!</h1>"
+    return render_template("user.html", name=name)
