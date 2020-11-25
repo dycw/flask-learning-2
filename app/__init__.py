dd69ec83f9da121d21feec418021381ae8536e7c
Derek Wan <d.wan@icloud.com>
2020-11-25 23:04:23 +0800

    Add config, forms and routes

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from __future__ import annotations
 
 from flask import Flask
 
+from config import Config
+
 
 __version__ = "0.0.3"
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 
 from app import routes  # noqa: E402
--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,32 @@ from __future__ import annotations
 from flask import render_template
 
 from app import app
+from app.forms import LoginForm
 
 
 @app.route("/")
 @app.route("/index")
 def index() -> str:
     user = {"username": "Miguel"}
-    return render_template("index.html", title="Home", user=user)
+    posts = [
+        {
+            "author": {"username": "John"},
+            "body": "Beautiful day in Portland!",
+        },
+        {
+            "author": {"username": "Susan"},
+            "body": "The Avengers movie was so cool!",
+        },
+    ]
+    return render_template(
+        "index.html",
+        title="Home",
+        user=user,
+        posts=posts,
+    )
+
+
+@app.route("/login")
+def login() -> str:
+    form = LoginForm()
+    return render_template("login.html", title="Sign In", form=form)
