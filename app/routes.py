9df19451ee1f3394c09b704ea20192b8c1dcb500
Derek Wan <d.wan@icloud.com>
2020-11-25 22:43:51 +0800

    Use render_template

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+from flask import render_template
+
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index() -> str:
-    return "Hello, World!"
+    user = {"username": "Miguel"}
+    return render_template("index.html", title="Home", user=user)
