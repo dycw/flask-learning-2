60c87f43b7eeff59a4683a6dd29a33fbfdad01df
Derek Wan <d.wan@icloud.com>
2020-11-26 01:08:10 +0800

    Re-enable ntipick

--- config.py
+++ config.py
@@ -1,7 +1,19 @@
 from __future__ import annotations
 
 import os
+from os import environ
+from pathlib import Path
+
+
+basedir = Path(__file__).resolve().parent
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+    SECRET_KEY = environ.get("SECRET_KEY") or "you-will-never-guess"
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get(
+            "DATABASE_URL",
+        )
+        or "sqlite:///{}".format(basedir.joinpath("app.db"))
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
