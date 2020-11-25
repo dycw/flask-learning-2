5345cd78ac25d4620adc178f1de567232b0d80a5
Derek Wan <d.wan@icloud.com>
2020-11-25 22:02:05 +0800

    Fix first load of microblog.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,3 +7,9 @@ __version__ = "0.0.3"
 
 
 app = Flask(__name__)
+
+
+from app import routes  # noqa: E402
+
+
+_ = routes
--- microblog.py
+++ microblog.py
@@ -1,1 +1,6 @@
 from __future__ import annotations
+
+from app import app
+
+
+_ = app
