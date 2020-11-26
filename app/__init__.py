5cf0731ab10b28fb1078686548e1ada4e7d93886
Derek Wan <d.wan@icloud.com>
2020-11-26 14:25:22 +0800

    Progress with Flask mega tutorial (#5)

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,23 @@
 from __future__ import annotations
 
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
+from app import models  # noqa: E402
 from app import routes  # noqa: E402
 
 
-_ = routes
+_ = (models, routes)
--- microblog.py
+++ microblog.py
@@ -1,6 +1,14 @@
 from __future__ import annotations
 
+from typing import Any
+from typing import Dict
+
 from app import app
+from app import db
+from app.models import Post
+from app.models import User
 
 
-_ = app
+@app.shell_context_processor
+def make_shell_context() -> Dict[str, Any]:
+    return {"db": db, "User": User, "Post": Post}
