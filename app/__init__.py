30a5aa6a3a954ff8e5881505b19cc4cd115c10bb
Derek Wan <d.wan@icloud.com>
2020-11-26 17:38:11 +0800

    Add user profile page (#6)

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,22 @@
 from __future__ import annotations
 
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = "login"
 
 
 from app import models  # noqa: E402
--- app/models.py
+++ app/models.py
@@ -1,11 +1,19 @@
 from __future__ import annotations
 
 from datetime import datetime
+from typing import Callable
+from typing import cast
+
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
 
 from app import db
+from app import login
+from utilities import T
 
 
-class User(db.Model):  # type: ignore
+class User(UserMixin, db.Model):  # type: ignore
     id = db.Column(db.Integer, primary_key=True)  # noqa: A003
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -14,6 +22,12 @@ class User(db.Model):  # type: ignore
 
     def __repr__(self: User) -> str:
         return f"<User {self.username}>"
+
+    def set_password(self: User, password: str) -> None:
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self: User, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):  # type: ignore
@@ -24,3 +38,8 @@ class Post(db.Model):  # type: ignore
 
     def __repr__(self: Post) -> str:
         return f"<Post {self.body}>"
+
+
+@cast(Callable[[T], T], login.user_loader)
+def load_user(id: str) -> User:  # noqa: A002
+    return User.query.get(int(id))
