ad958873a6af9b6f106b9763d95e93592e85d271
Derek Wan <d.wan@icloud.com>
2020-11-26 20:21:10 +0800

    Add about_me & last_seen to User

--- app/models.py
+++ app/models.py
@@ -20,6 +20,8 @@ class User(UserMixin, db.Model):  # type: ignore
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship("Post", backref="author", lazy="dynamic")
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self: User) -> str:
         return f"<User {self.username}>"
