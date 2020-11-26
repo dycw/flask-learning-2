75f7ad1213b49161edd2460c3629891953e3b714
Derek Wan <d.wan@icloud.com>
2020-11-26 09:21:39 +0800

    Add the post model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from datetime import datetime
+
 from app import db
 
 
@@ -8,6 +10,17 @@ class User(db.Model):  # type: ignore
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship("Post", backref="author", lazy="dynamic")
 
     def __repr__(self: User) -> str:
         return f"<User {self.username}>"
+
+
+class Post(db.Model):  # type: ignore
+    id = db.Column(db.Integer, primary_key=True)  # noqa: A003
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __repr__(self: Post) -> str:
+        return f"<Post {self.body}>"
