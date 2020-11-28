704776b261fa8646d1711ede0974be17912abf86
Derek Wan <d.wan@icloud.com>
2020-11-28 17:49:19 +0800

    Add sending of gmail

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import os
 from os import environ
 from pathlib import Path
 
@@ -11,17 +10,15 @@ basedir = Path(__file__).resolve().parent
 class Config:
     SECRET_KEY = environ.get("SECRET_KEY") or "you-will-never-guess"
     SQLALCHEMY_DATABASE_URI = (
-        os.environ.get(
+        environ.get(
             "DATABASE_URL",
         )
         or "sqlite:///{}".format(basedir.joinpath("app.db"))
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get("MAIL_SERVER")
-    MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
-    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
-    MAIL_USERNAME = (
-        os.environ.get("MAIL_USERNAME") or "derek.wan.test2@gmail.com"
-    )
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD") or "lewis wet sucks luke"
+    MAIL_SERVER = environ.get("MAIL_SERVER") or "smtp.googlemail.com"
+    MAIL_PORT = int(environ.get("MAIL_PORT") or 587)
+    MAIL_USE_TLS = (environ.get("MAIL_USE_TLS") is not None) or True
+    MAIL_USERNAME = environ.get("MAIL_USERNAME") or "derek.wan.test2@gmail.com"
+    MAIL_PASSWORD = environ.get("MAIL_PASSWORD") or "lewis wet sucks luke"
     ADMINS = ["d.wan@icloud.com"]
