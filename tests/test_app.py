5e69991adb31d9321863a1cb06909fc54b922260
Derek Wan <d.wan@icloud.com>
2020-11-28 22:34:11 +0800

    Test avatar too

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,3 +8,11 @@ def test_password_hashing() -> None:
     u.set_password("cat")
     assert not u.check_password("dog")
     assert u.check_password("cat")
+
+
+def test_avatar() -> None:
+    u = User(username="john", email="john@example.com")
+    assert u.avatar(128) == (
+        "https://www.gravatar.com/avatar/"
+        "d4c74594d841139328695756648b6bd6?d=identicon&s=128"
+    )
