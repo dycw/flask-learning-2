2e743e4dfddc63cda8b17900ce4754bd4d1ae456
Derek Wan <d.wan@icloud.com>
2020-11-28 18:52:30 +0800

    EditProfileForm now has original username checked against

--- app/routes.py
+++ app/routes.py
@@ -113,7 +113,7 @@ def before_request() -> None:
 @app.route("/edit_profile", methods=["GET", "POST"])
 @cast(Callable[[T], T], login_required)
 def edit_profile() -> Union[str, Response]:
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
