a74aa5fb24d9005a47f8755ed88c62df20502d93
Derek Wan <d.wan@icloud.com>
2020-11-28 22:32:43 +0800

    Ignore logs/ too, looponfailroots specified

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,10 @@
 from __future__ import annotations
 
+from app.models import User
 
-def test_main() -> None:
-    assert True
+
+def test_password_hashing() -> None:
+    u = User(username="susan")
+    u.set_password("cat")
+    assert not u.check_password("dog")
+    assert u.check_password("cat")
